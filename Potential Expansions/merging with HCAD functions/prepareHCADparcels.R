e1bb3670d50f3861903d9a75a26788db52a637a1
LAPTOP-JBNCS8L6\Carol <phantom1994@sbcglobal.net>
2018-04-03 08:32:02 -0500

    Yay! I have more buildings!!!

--- Potential Expansions/merging with HCAD functions/prepareHCADparcels.R
+++ Potential Expansions/merging with HCAD functions/prepareHCADparcels.R
@@ -17,6 +17,8 @@ CensusTractforHCADParcelsunlisted=unlist(CensusTractforHCADParcelsunlisted)
 validparcels$COUNTY=TXCensusTracts$COUNTY[CensusTractforHCADParcelsunlisted]
 validparcels$TRACT=TXCensusTracts$TRACT[CensusTractforHCADParcelsunlisted]
 
+#Save this thing because it takes a long time to load
+saveRDS(validparcels,"validparcels.RDS")
 #Merge with building features for residential houses
 
 #Read in data
@@ -41,6 +43,7 @@ validparceldataframe2=as.data.frame(validparcel2)
 #csv is a bad idea because of geometry column
 
 saveRDS(validparceldataframe2,"validparceldataframe2.RDS")
+saveRDS(validparcel2,"validparcelsforhouseholds.RDS")
 #Write to tab delimited file
 
 #So places where people may live in for group quarters I think are more likely to be in the commercial file
@@ -48,16 +51,14 @@ saveRDS(validparceldataframe2,"validparceldataframe2.RDS")
 
 #So we need the non residential files
 buildingfeatures=read.table('../hcadparcelstuff/building_other.txt',sep="\t", header=FALSE, fill=TRUE,colClasses = "character",strip.white = TRUE,quote = "")#, ,colClasses = "character",stringsAsFactors = FALSE
-colnames(buildingfeatures)=c("ACCOUNT", "USE_CODE", "BUILDING_NUMBER","IMPRV_TYPE","BUILDING_STYLE_CODE","CLASS_STRUCTURE","CLASS_STRUC_DESCRIPTION","DEPRECIATION_VALUE","CAMA_REPLACEMENT_COST","ACCRUED_DEPR_PCT","QUALITY","QUALITY_DESCRIPTION","DATE_ERECTED","EFFECTIVE_DATE","YR_REMODEL","YR_ROLL","APPRAISED_BY","APPRAISED_DATE","NOTE","IMPR_SQ_FT","ACTUAL_AREA","HEAT_AREA","GROSS_AREA","EFFECTIVE_AREA","BASE_AREA","PERIMETER","PERCENT_COMPLETE","NBHD_FACTOR","RCNLD","SIZE_INDEX","LUMP_SUM_ADJ")
+colnames(buildingfeatures)=c("ACCOUNT", "USE_CODE", "BUILDING_NUMBER","IMPRV_TYPE","BUILDING_STYLE_CODE","CLASS_STRUCTURE","CLASS_STRUC_DESCRIPTION","NOTICED_DEPR_VALUE","DEPRECIATION_VALUE","MS_REPLACEMENT_COST","CAMA_REPLACEMENT_COST","ACCRUED_DEPR_PCT","QUALITY","QUALITY_DESCRIPTION","DATE_ERECTED","EFFECTIVE_DATE","YR_REMODEL","YR_ROLL","APPRAISED_BY","APPRAISED_DATE","NOTE","IMPR_SQ_FT","ACTUAL_AREA","HEAT_AREA","GROSS_AREA","EFFECTIVE_AREA","BASE_AREA","PERIMETER","PERCENT_COMPLETE","CATEGORY","CATEGORY_DESC","PROPERTY_NAME","UNITS","NET_RENT_AREA","LEASE_RATE","OCCUPANCY","TOTAL_INCOME")
 
-#Maybe this is better than that
-library(tigris)
 buildingfeatures$"HCAD_NUM"=buildingfeatures$ACCOUNT
 
-validparcel3=geo_join(validparcels,buildingfeatures,by="HCAD_NUM",how="inner")
-
+validparcels3=geo_join(validparcels,buildingfeatures,by="HCAD_NUM",how="inner")
+saveRDS(validparcels3,"validparcelsforgroupquartersmaybe.RDS")
 #Convert to data frame
-validparceldataframe3=as.data.frame(validparcel2)
+validparceldataframe3=as.data.frame(validparcel3)
 #validparceldataframe2=unlist(validparceldataframe2)
 #Write to tab delimited file
 #csv is a bad idea because of geometry column
