21d461a5beeebf53057e24ffec245257a30a8e7e
mhenini <35389619+mhenini@users.noreply.github.com>
2018-06-28 19:44:06 -0500

    Update move_out_by_tract.R
    Moves people out based off of which age group has the largest difference

--- Potential Expansions/aging functions to be/move_out_by_tract.R
+++ Potential Expansions/aging functions to be/move_out_by_tract.R
@@ -38,249 +38,40 @@ move_out_by_tract<-function(sample_set,Census_data,tract,seed){
   
   differences=current_data_for_update[variables_of_interest]-our_people[variables_of_interest]
   
+  MinTable <- function(x){
+    return(which.min(apply(x, MARGIN=2,min)))
+  }
+  
   #If we have too many people move people out until same amount of people
   while(sum(differences)<0){
     set.seed(seed)
-    if(differences$same.house.over.75<0){
-      indexes_potential_75_year_olds_to_move=which(people_still_living_in_the_tract$real_age>=75)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_75_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
+    
+    max_diff=MinTable(differences)
+    
+    min_age = switch(as.character(max_diff),
+                     "2" = 5,"3" = 18, "4"=20, "5"=25, "6"=30, "7"=35,"8"=40,"9"=45,"10"=50,"11"=55,"12"=60,"13"=65,"14"=70,"15"=75,-1)
+    max_age = switch(as.character(max_diff),
+                     "1"=5,"2"=17,"3"=19,"4"=24,"5"=29,"6"=34,"7"=39,"8"=44,"9"=49,"10"=54,"11"=59,"12"=64,"13"=69,"14"=74,105)
+    
+    while(max_diff==MinTable(differences)){
+      if(differences[max_diff]<0){
+        indexes_potential_to_move=which(people_still_living_in_the_tract$real_age<as.numeric(max_age)&people_still_living_in_the_tract$real_age>=as.numeric(min_age))
+        #Move someone out
+        who_moves_out=sample(indexes_potential_to_move,size=1)
+        people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
+        #update household size if not group quarters
+        if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
+          hhID=people_still_living_in_the_tract$householdID[who_moves_out]
+          size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
+          people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
+        }
+        #update differences
+        differences[max_diff]=differences[max_diff]+1
+        #change seed
+        seed=seed+1
       }
-      #update differences
-      differences$same.house.over.75=differences$same.house.over.75+1
-      #change seed
-      seed=seed+1
     }
-    if(differences$same.house.70.to.74<0){
-      indexes_potential_70_year_olds_to_move=which(people_still_living_in_the_tract$real_age<75&people_still_living_in_the_tract$real_age>=70)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_70_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.70.to.74=differences$same.house.70.to.74+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.65.to.69<0){
-      indexes_potential_65_year_olds_to_move=which(people_still_living_in_the_tract$real_age<70&people_still_living_in_the_tract$real_age>=65)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_65_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.65.to.69=differences$same.house.65.to.69+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.60.to.64<0){
-      indexes_potential_60_year_olds_to_move=which(people_still_living_in_the_tract$real_age<65&people_still_living_in_the_tract$real_age>=60)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_60_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.60.to.64=differences$same.house.60.to.64+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.55.to.59<0){
-      indexes_potential_55_year_olds_to_move=which(people_still_living_in_the_tract$real_age<60&people_still_living_in_the_tract$real_age>=55)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_55_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.55.to.59=differences$same.house.55.to.59+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.50.to.54<0){
-      indexes_potential_50_year_olds_to_move=which(people_still_living_in_the_tract$real_age<55&people_still_living_in_the_tract$real_age>=50)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_50_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.50.to.54=differences$same.house.50.to.54+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.45.to.49<0){
-      indexes_potential_45_year_olds_to_move=which(people_still_living_in_the_tract$real_age<50&people_still_living_in_the_tract$real_age>=45)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_45_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.45.to.49=differences$same.house.45.to.49+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.40.to.44<0){
-      indexes_potential_40_year_olds_to_move=which(people_still_living_in_the_tract$real_age<45&people_still_living_in_the_tract$real_age>=40)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_40_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.40.to.44=differences$same.house.40.to.44+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.35.to.39<0){
-      indexes_potential_35_year_olds_to_move=which(people_still_living_in_the_tract$real_age<40&people_still_living_in_the_tract$real_age>=35)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_35_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.35.to.39=differences$same.house.35.to.39+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.30.to.34<0){
-      indexes_potential_30_year_olds_to_move=which(people_still_living_in_the_tract$real_age<35&people_still_living_in_the_tract$real_age>=30)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_30_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.30.to.34=differences$same.house.30.to.34+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.25.to.29<0){
-      indexes_potential_25_year_olds_to_move=which(people_still_living_in_the_tract$real_age<30&people_still_living_in_the_tract$real_age>=25)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_25_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.25.to.29=differences$same.house.25.to.29+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.20.to.24<0){
-      indexes_potential_20_year_olds_to_move=which(people_still_living_in_the_tract$real_age<25&people_still_living_in_the_tract$real_age>=20)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_20_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.20.to.24=differences$same.house.20.to.24+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.18.to.19<0){
-      indexes_potential_18_year_olds_to_move=which(people_still_living_in_the_tract$real_age<20&people_still_living_in_the_tract$real_age>=18)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_18_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.18.to.19=differences$same.house.18.to.19+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.5.to.17<0){
-      indexes_potential_17_year_olds_to_move=which(people_still_living_in_the_tract$real_age<18&people_still_living_in_the_tract$real_age>=5)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_17_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.5.to.17=differences$same.house.5.to.17+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
-    if(differences$same.house.under.5<0){
-      indexes_potential_5_year_olds_to_move=which(people_still_living_in_the_tract$real_age<5)
-      #Move someone out
-      who_moves_out=sample(indexes_potential_5_year_olds_to_move,size=1)
-      people_still_living_in_the_tract$Moved_Out[who_moves_out]="Moved Out"
-      #update differences
-      differences$same.house.under.5=differences$same.house.under.5+1
-      #update household size if not group quarters
-      if(people_still_living_in_the_tract$household.type[who_moves_out]!="Group Quarters"){
-        hhID=people_still_living_in_the_tract$householdID[who_moves_out]
-        size=as.numeric(people_still_living_in_the_tract$size[who_moves_out])+1
-        people_still_living_in_the_tract$size[people_still_living_in_the_tract$householdID==hhID]=size
-      }
-      #change seed
-      seed=seed+1
-    }
+    
     newly_moved_out=subset(people_still_living_in_the_tract,people_still_living_in_the_tract$Moved_Out=="Moved Out")
     people_that_moved_out=rbind(people_that_moved_out,newly_moved_out)
     people_still_living_in_the_tract=subset(people_still_living_in_the_tract,is.na(people_still_living_in_the_tract$Moved_Out))
