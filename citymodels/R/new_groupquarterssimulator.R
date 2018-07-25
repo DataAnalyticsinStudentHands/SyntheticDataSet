10345dbf5fef02774c95cea385e53e44adda1005
mhenini <35389619+mhenini@users.noreply.github.com>
2018-07-25 17:19:50 -0500

    Update new_groupquarterssimulator.R

--- citymodels/R/new_groupquarterssimulator.R
+++ citymodels/R/new_groupquarterssimulator.R
@@ -22,62 +22,58 @@ group_quarters_simulater <- function(state, county, tract, seed, inputdir = "../
   
   number.of.people = Census_data$group.quarters.population
 
+   #Make Sure tract picked actually has people in it
   if(number.of.people > 0){
     #Create a seed for each household from original seed
     seeds = sample(1:100000000, number.of.people, replace = FALSE)
     #create empty data set to build from
 
     fullset = as.data.frame(do.call(rbind, lapply(seeds, function(seedy){
-    #Make Sure tract picked actually has people in it
-      total1 = number.of.people
-      if (total1 > 0){
-        #Begin sampling characteristics of household (functions stored in other scripts)
-        #The functions must be called in this order as some characteristics have different probability distributions based on other characteristics
+       #Begin sampling characteristics of household (functions stored in other scripts)
+       #The functions must be called in this order as some characteristics have different probability distributions based on other characteristics
 
-        #Build using Census Data
-        partofset=data.frame(household.type="Group Quarters",members="NA",size="Group Quarters") #create initial data frame
-        partofset = getindividualcharacteristics(partofset, seedy, Census_data) #simulates sex, race, age, school.enrollment, education.attainment, employment, disability, nativity, citizenship, language, veteran.status, transport.method, travel.time
-        partofset$number.of.vehicles = getnumberofvehiclesforgroupquarters(Census_data, seedy, partofset) #depends on sex and employment
-        partofset$household.income = getincomeforgroupquarters(Census_data, seedy, partofset) #independent  -- samples are directly from census data
-        partofset$health.insurance = gethealthinsuranceforgroupquarters(Census_data, seedy, partofset) #depends on disability and age
-        partofset$bracket.age=NULL #this column is no longer needed
+       #Build using Census Data
+       partofset=data.frame(household.type="Group Quarters",members="NA",size="Group Quarters") #create initial data frame
+       partofset = getindividualcharacteristics(partofset, seedy, Census_data) #simulates sex, race, age, school.enrollment, education.attainment, employment, disability, nativity, citizenship, language, veteran.status, transport.method, travel.time
+       partofset$number.of.vehicles = getnumberofvehiclesforgroupquarters(Census_data, seedy, partofset) #depends on sex and employment
+       partofset$household.income = getincomeforgroupquarters(Census_data, seedy, partofset) #independent  -- samples are directly from census data
+       partofset$health.insurance = gethealthinsuranceforgroupquarters(Census_data, seedy, partofset) #depends on disability and age
+       partofset$bracket.age=NULL #this column is no longer needed
 
-        #Build Using 500 Cities Project Data
-        #partofset=get65menuptodate(county,tract,partofset,seedy)
-        #partofset=get65womenuptodate(county,tract,partofset,seedy)
-        #partofset=getadultasthma(county,tract,partofset,seedy)
-        #partofset=getarthritis(county,tract,partofset,seedy)
-        #partofset=getbingedrinking(county,tract,partofset,seedy)
-        #partofset=getcancer(county,tract,partofset,seedy)
-        #partofset=getcholesterolscreening(county,tract,partofset,seedy)
-        #partofset=getchronicobspulmonarydisease(county,tract,partofset,seedy)
-        #partofset=getcolonoscopy(county,tract,partofset,seedy)
-        #partofset=getcoronaryheartdisease(county,tract,partofset,seedy)
-        #partofset=getdiabetes(county,tract,partofset,seedy)
-        #partofset=gethighbloodpressure(county,tract,partofset,seedy)
-        #partofset=gethbpmedications(county,tract,partofset,seedy)
-        #partofset=gethighcholesterol(county,tract,partofset,seedy)
-        #partofset=getkidneydisease(county,tract,partofset,seedy)
-        #partofset=getmammographyuse(county,tract,partofset,seedy)
-        #partofset=getmentalhealth(county,tract,partofset,seedy)
-        #partofset=getnoleisuretime(county,tract,partofset,seedy)
-        #partofset=getobesity(county,tract,partofset,seedy)
-        #partofset=getpapsmear(county,tract,partofset,seedy)
-        #partofset=getphysicalhealth(county,tract,partofset,seedy)
-        #partofset=getroutinecheckups(county,tract,partofset,seedy)
-        #partofset=getsleep(county,tract,partofset,seedy)
-        #partofset=getsmokers(county,tract,partofset,seedy)
-        #partofset=getstroke(county,tract,partofset,seedy)
-        #partofset=getteeth(county,tract,partofset,seedy)
-        partofset$state = rep(state,nrow(partofset))
-        partofset$county = rep(county,nrow(partofset))
-        partofset$tract = rep(tract,nrow(partofset))
+       #Build Using 500 Cities Project Data
+       #partofset=get65menuptodate(county,tract,partofset,seedy)
+       #partofset=get65womenuptodate(county,tract,partofset,seedy)
+       #partofset=getadultasthma(county,tract,partofset,seedy)
+       #partofset=getarthritis(county,tract,partofset,seedy)
+       #partofset=getbingedrinking(county,tract,partofset,seedy)
+       #partofset=getcancer(county,tract,partofset,seedy)
+       #partofset=getcholesterolscreening(county,tract,partofset,seedy)
+       #partofset=getchronicobspulmonarydisease(county,tract,partofset,seedy)
+       #partofset=getcolonoscopy(county,tract,partofset,seedy)
+       #partofset=getcoronaryheartdisease(county,tract,partofset,seedy)
+       #partofset=getdiabetes(county,tract,partofset,seedy)
+       #partofset=gethighbloodpressure(county,tract,partofset,seedy)
+       #partofset=gethbpmedications(county,tract,partofset,seedy)
+       #partofset=gethighcholesterol(county,tract,partofset,seedy)
+       #partofset=getkidneydisease(county,tract,partofset,seedy)
+       #partofset=getmammographyuse(county,tract,partofset,seedy)
+       #partofset=getmentalhealth(county,tract,partofset,seedy)
+       #partofset=getnoleisuretime(county,tract,partofset,seedy)
+       #partofset=getobesity(county,tract,partofset,seedy)
+       #partofset=getpapsmear(county,tract,partofset,seedy)
+       #partofset=getphysicalhealth(county,tract,partofset,seedy)
+       #partofset=getroutinecheckups(county,tract,partofset,seedy)
+       #partofset=getsleep(county,tract,partofset,seedy)
+       #partofset=getsmokers(county,tract,partofset,seedy)
+       #partofset=getstroke(county,tract,partofset,seedy)
+       #partofset=getteeth(county,tract,partofset,seedy)
+       partofset$state = rep(state,nrow(partofset))
+       partofset$county = rep(county,nrow(partofset))
+       partofset$tract = rep(tract,nrow(partofset))
+       partofset$householdID = rep(paste("group_quarters", county, tract, seedy, sep=".", collapse="."), nrow(partofset))
 
-        partofset$householdID = rep(paste("group_quarters", county, tract, seedy, sep=".", collapse="."), nrow(partofset))
-
-        #Save everyone together
-        return(partofset)
-      }
+       #Save everyone together
+       return(partofset)
     })))
   }
 
