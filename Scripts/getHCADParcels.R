ba07c561dafe0da42894b3eb58de6115fbd688ba
mhenini <35389619+mhenini@users.noreply.github.com>
2019-03-05 22:52:42 -0600

    Update getHCADParcels.R

--- Scripts/getHCADParcels.R
+++ Scripts/getHCADParcels.R
@@ -1,5 +1,7 @@
 getHCADParcels <- function(){
   library(sf)
+  
+  # The Parcels.shp file can be downloaded from http://pdata.hcad.org/GIS/index.html
   parcels <- st_read("Parcels.shp")
 
   parcels$valid=st_is_valid(parcels, reason = TRUE)
@@ -23,13 +25,9 @@ getHCADParcels <- function(){
   #Merge with building features for residential houses
 
   #Read in data
+  # The "building_res.txt" file can be downloaded from http://pdata.hcad.org/download/2014.html
   buildingfeatures=read.table('building_res.txt',sep="\t", header=FALSE, fill=TRUE,colClasses = "character",strip.white = TRUE,quote = "")#, ,colClasses = "character",stringsAsFactors = FALSE
-  #buildingfeatures=buildingfeatures[-(1:371770),]
   colnames(buildingfeatures)=c("ACCOUNT", "USE_CODE", "BUILDING_NUMBER","IMPRV_TYPE","BUILDING_STYLE_CODE","CLASS_STRUCTURE","CLASS_STRUC_DESCRIPTION","DEPRECIATION_VALUE","CAMA_REPLACEMENT_COST","ACCRUED_DEPR_PCT","QUALITY","QUALITY_DESCRIPTION","DATE_ERECTED","EFFECTIVE_DATE","YR_REMODEL","YR_ROLL","APPRAISED_BY","APPRAISED_DATE","NOTE","IMPR_SQ_FT","ACTUAL_AREA","HEAT_AREA","GROSS_AREA","EFFECTIVE_AREA","BASE_AREA","PERIMETER","PERCENT_COMPLETE","NBHD_FACTOR","RCNLD","SIZE_INDEX","LUMP_SUM_ADJ")
-
-  #validparceldataframe=as.data.frame(validparcels)
-
-  #validparceldataframe2=merge(buildingfeatures,validparceldataframe,by.x="ACCOUNT",by.y="HCAD_NUM")
 
   #Maybe this is better than that
   library(tigris)
@@ -39,7 +37,7 @@ getHCADParcels <- function(){
 
   #Convert to data frame
   validparceldataframe2=as.data.frame(validparcel2)
-  #validparceldataframe2=unlist(validparceldataframe2)
+  
   #Write to tab delimited file
   #csv is a bad idea because of geometry column
 
@@ -51,6 +49,7 @@ getHCADParcels <- function(){
   #then residential because it's commercial property-dorms, assisted living facilities etc
 
   #So we need the non residential files
+  # The "building_other.txt" file can be downloaded from http://pdata.hcad.org/download/2014.html
   buildingfeatures=read.table('building_other.txt',sep="\t", header=FALSE, fill=TRUE,colClasses = "character",strip.white = TRUE,quote = "")#, ,colClasses = "character",stringsAsFactors = FALSE
   colnames(buildingfeatures)=c("ACCOUNT", "USE_CODE", "BUILDING_NUMBER","IMPRV_TYPE","BUILDING_STYLE_CODE","CLASS_STRUCTURE","CLASS_STRUC_DESCRIPTION","NOTICED_DEPR_VALUE","DEPRECIATION_VALUE","MS_REPLACEMENT_COST","CAMA_REPLACEMENT_COST","ACCRUED_DEPR_PCT","QUALITY","QUALITY_DESCRIPTION","DATE_ERECTED","EFFECTIVE_DATE","YR_REMODEL","YR_ROLL","APPRAISED_BY","APPRAISED_DATE","NOTE","IMPR_SQ_FT","ACTUAL_AREA","HEAT_AREA","GROSS_AREA","EFFECTIVE_AREA","BASE_AREA","PERIMETER","PERCENT_COMPLETE","CATEGORY","CATEGORY_DESC","PROPERTY_NAME","UNITS","NET_RENT_AREA","LEASE_RATE","OCCUPANCY","TOTAL_INCOME")
 
@@ -60,36 +59,12 @@ getHCADParcels <- function(){
   saveRDS(validparcels3,"validparcelsforgroupquartersmaybe.RDS")
   #Convert to data frame
   validparceldataframe3=as.data.frame(validparcels3)
-  #validparceldataframe2=unlist(validparceldataframe2)
+  
   #Write to tab delimited file
   #csv is a bad idea because of geometry column
 
   saveRDS(validparceldataframe3,"validparceldataframe3forgroupquarters.RDS")
 
-  #Building Style Codes of Interest
-  #8351 Single Family Residence
-  #8150 Single Wide Commercial Home
-  #101 Residential 1 Family
-  #102 Residential 2 Family
-  #103 Residential 3 Family
-  #104 Residential 4 Family or More
-  #108 Single Wide Residential Mobile Home
-  #109 Double ide Reidential Mobile Home
-  #125 Farm with Dwelling
-  #8300 Apartment
-  #8321 Dormitory
-  #8324 Frat House
-  #8338 Loft
-  #8352 Multiple Res (Low Rise)
-  #8424 Group Care Home
-  #8451 Multiple Res (en. Citizen)
-  #8459 Mixed Retail with Resid. Units
-  #8587 Shell Multiple Res
-  #8589 Elderly Assist Multi Res
-  #8596 Shell, Apartment
-  #8710 Retirement Community Complex
-
-  #
   vp1=readRDS("validparcelsforhouseholds.RDS")
   vp2=readRDS("validparcelsforgroupquartersmaybe.RDS")
   library(plyr)
