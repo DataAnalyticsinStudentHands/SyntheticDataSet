a2cf7cdee61dfe80880d0ef5a28a5c4fa572a58c
mhenini <35389619+mhenini@users.noreply.github.com>
2019-03-18 10:06:08 -0500

    Update main.R

--- Scripts/main.R
+++ Scripts/main.R
@@ -13,6 +13,16 @@ validparceldataframe2 = getHCADParcels()
 validparceldataframe2$ACCOUNT = paste0(validparceldataframe2$ACCOUNT, "_", validparceldataframe2$BUILDING_NUMBER)
 validparceldataframe2 = validparceldataframe2[!duplicated(validparceldataframe2$ACCOUNT), ]
 
+# From the buildings in the tract, subset for only the ones with living spaces
+potential_buildings=subset(validparceldataframe2,validparceldataframe2$"BUILDING_STYLE_CODE" %in% c("101","102", "103", "104", "107","108","109","125","8177","8178","8179","8351","8354","8401","8548","8549","8550","8986","8988","105","8300","8352","8338","8459","8493","8546","8547","8596","8984","8987","8989", "660","8321","8324","8393","8424","8451","8589","8313","8322","8330","8335","8348","8394","8156","8551","8588","8710","8331","8309","8489","8311","8491","8514"))
+
+# Depending on the USE_CODE some buildings will only have one unit while others will have multiple units. If a building doesn't list units then use the average number of units for that building's USE_CODE
+potential_buildings$UNITS[potential_buildings$USE_CODE %in% c("1D1", "A1", "A2", "A3", "A4", "B2", "B3", "B4", "C1", "C2", "C3", "E1", "M3", "O1", "O2", "U0", "X4", "XG", "XI", "XL", "Z4") | is.na(potential_buildings$UNITS)]= 1
+for(x in c("B1", "F1", "F2", "X1", "X2", "X3", "XE", "XJ", "XU")){
+   avg_units = round(mean(as.integer(potential_buildings[potential_buildings$USE_CODE == x & !potential_buildings$UNITS %in% c("", "A", "B", "C", "D", "1/2", "1/1"),]$UNITS), na.rm = T))
+   potential_buildings$UNITS[potential_buildings$USE_CODE == x & potential_buildings$UNITS %in% c("", "A", "B", "C", "D", "1/1", "1/2")] = avg_units
+}
+
 # Everything else will be parallelized by the tract numbers
 tracts = subset(Census_data, Census_data$county == 201)
 tracts = tracts$tract
@@ -51,23 +61,15 @@ saveRDS(prop, "proportionCheck.R")
 # Now the people from the simulated model will be placed in households in the HCAD data
 complete_sample_set=foreach (index = 1:786,.combine='rbind')%dopar%{
   # Subset for the proper buildings and individuals based on tract
-  tracthouses = subset(validparceldataframe2,validparceldataframe2$TRACT==tracts[index])
-  tract.sample.set = subset(sample.set,sample.set$tract==tracts[index])
+  tracthouses=subset(potential_buildings, potential_buildings$TRACT == tracts[index])
+  tract.sample.set = subset(sample.set, sample.set$tract == tracts[index])
 
   # store groupquarters and household IDs from the simulated model
   group_quartersIDs = unique(subset(sample.set,sample.set$tract==tracts[index]&sample.set$household.type=="Group Quarters")$householdID)
   householdIDs = unique(subset(sample.set,sample.set$tract==tracts[index]&sample.set$household.type!="Group Quarters")$householdID)
 
-  # From the buildings in the tract, subset for only the ones with living spaces
-  potential_buildings = subset(tracthouses,tracthouses$"BUILDING_STYLE_CODE" %in% c("101","102", "103", "104", "107","108","109","125","8177","8178","8179","8351","8354","8401","8548","8549","8550","8986","8988","105","8300","8352","8338","8459","8493","8546","8547","8596","8984","8987","8989", "660","8321","8324","8393","8424","8451","8589","8313","8322","8330","8335","8348","8394","8156","8551","8588","8710","8331","8309","8489","8311","8327","8491","8514"))
-  
-  # Find the average number of units the multiple-residence buildings have. Replace units that are blank or have letters with the average. Replace units that are NA or fractions with 1.
-  avg_units = round(mean(as.integer(potential_buildings[!potential_buildings$UNITS %in% c("", "A", "B", "C", "D", "1/2", "1/1"),]$UNITS), na.rm = T))
-  potential_buildings$UNITS[is.na(potential_buildings$UNITS) | potential_buildings$UNITS %in% c("1/1", "1/2")] = 1
-  potential_buildings$UNITS[potential_buildings$UNITS %in% c("", "A", "B", "C", "D")] = avg_units
-
-  # From the potential buildings, subset for only buildings not used for group quarters
-  houses = potential_buildings[potential_buildings$BUILDING_STYLE_CODE %in% c("101","102", "103", "104", "107","108","109","125","8177","8178","8179","8351","8354","8401","8548","8549","8550","8986","8988","105","8300","8352","8338","8459","8493","8546","8547","8596","8984","8987","8989" ),]
+  # From the potential buildings in the tract, subset for only buildings not used for group quarters
+  houses = tracthouses[tracthouses$BUILDING_STYLE_CODE %in% c("101","102", "103", "104", "107","108","109","125","8177","8178","8179","8351","8354","8401","8548","8549","8550","8986","8988","105","8300","8352","8338","8459","8493","8546","8547","8596","8984","8987","8989" ),]
   
   # If there are no households available in this tract then remove the household IDs from the tract. Otherwise place people in houses until there are either no more locations or no more individuals
   if(length(houses$ACCOUNT) == 0 & length(householdIDs) > 0){
@@ -96,8 +98,8 @@ complete_sample_set=foreach (index = 1:786,.combine='rbind')%dopar%{
     }
   }
   
-   # From the potential buildings, subset for only buildings used for group quarters
-  groupquartersplaces=subset(potential_buildings,(potential_buildings$"BUILDING_STYLE_CODE" %in% c("660","8321","8324","8393","8424","8451","8589","8313","8322","8330","8335","8348","8394","8156","8551","8588","8710","8331","8309","8489","8311","8327","8491","8514")))
+  # From the potential buildings, subset for only buildings used for group quarters
+  groupquartersplaces=subset(tracthouses,(tracthouses$"BUILDING_STYLE_CODE" %in% c("660","8321","8324","8393","8424","8451","8589","8313","8322","8330","8335","8348","8394","8156","8551","8588","8710","8331","8309","8489","8311","8491","8514")))
  
   # Repeat the same steps as before but with groupquartersplaces and group_quartersIDs
   if(length(groupquartersplaces$ACCOUNT) == 0 & length(group_quartersIDs) > 0){
