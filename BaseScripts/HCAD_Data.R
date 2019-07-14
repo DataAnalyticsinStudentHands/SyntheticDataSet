7e70ed6125eccfffcfac08f3944f202a09a2f09f
Peggy Lindner <peggylind@gmail.com>
2019-07-14 16:00:05 -0500

    - new version of HCAD functions, still need to add filter for building style codes

--- BaseScripts/HCAD_Data.R
+++ BaseScripts/HCAD_Data.R
@@ -1,86 +1,113 @@
 library(sf)
+library(reader)
+library(tigris)
 
-# Make sure the required files downlaoded and in the proper directory before running the script. Some files may have to be downloaded from http://pdata.hcad.org/GIS/index.html and http://pdata.hcad.org/download/2014.html . Check the script for more details.
-
-getHCADParcels <- function(){
-  # The Parcels.shp file can be downloaded from http://pdata.hcad.org/GIS/index.html
-  parcels <- st_read("Parcels.shp")
-
-  parcels$valid=st_is_valid(parcels, reason = TRUE)
-  validparcels=subset(parcels,parcels$valid=="Valid Geometry")
-
-  #Read in Texas Census Tract Files
-  TXCensusTracts <- st_read("/Data/gz_2010_48_140_00_500k.shp")
-  #Put them in same CRS as Parcels
-  TXCensusTracts <- st_transform(TXCensusTracts,st_crs(validparcels))
-
-  #Get Census Tracts for each parcel
-  CensusTractforHCADParcels=st_within(validparcels,TXCensusTracts)
-  CensusTractforHCADParcelsunlisted=rapply(CensusTractforHCADParcels,function(x) ifelse(length(x)==0,9999999999999999999,x), how = "replace")
-  CensusTractforHCADParcelsunlisted=unlist(CensusTractforHCADParcelsunlisted)
-
-  validparcels$COUNTY=TXCensusTracts$COUNTY[CensusTractforHCADParcelsunlisted]
-  validparcels$TRACT=TXCensusTracts$TRACT[CensusTractforHCADParcelsunlisted]
-
-  #Save this thing because it takes a long time to load
-  saveRDS(validparcels,"validparcels.RDS")
-  #Merge with building features for residential houses
-
-  #Read in data
-  # The "building_res.txt" file can be downloaded from http://pdata.hcad.org/download/2014.html
-  buildingfeatures=read.table('building_res.txt',sep="\t", header=FALSE, fill=TRUE,colClasses = "character",strip.white = TRUE,quote = "")#, ,colClasses = "character",stringsAsFactors = FALSE
-  colnames(buildingfeatures)=c("ACCOUNT", "USE_CODE", "BUILDING_NUMBER","IMPRV_TYPE","BUILDING_STYLE_CODE","CLASS_STRUCTURE","CLASS_STRUC_DESCRIPTION","DEPRECIATION_VALUE","CAMA_REPLACEMENT_COST","ACCRUED_DEPR_PCT","QUALITY","QUALITY_DESCRIPTION","DATE_ERECTED","EFFECTIVE_DATE","YR_REMODEL","YR_ROLL","APPRAISED_BY","APPRAISED_DATE","NOTE","IMPR_SQ_FT","ACTUAL_AREA","HEAT_AREA","GROSS_AREA","EFFECTIVE_AREA","BASE_AREA","PERIMETER","PERCENT_COMPLETE","NBHD_FACTOR","RCNLD","SIZE_INDEX","LUMP_SUM_ADJ")
-
-  #Maybe this is better than that
-  library(tigris)
-  buildingfeatures$"HCAD_NUM"=buildingfeatures$ACCOUNT
-
-  validparcel2=geo_join(validparcels,buildingfeatures,by="HCAD_NUM",how="inner")
-
-  #Convert to data frame
-  validparceldataframe2=as.data.frame(validparcel2)
+#' getHCADParcels function
+#'
+#' This function reads parcels from HCAD (Harris County Aprraisal District), adds tracts information (county, tract) and remove unnecessary columns.
+#'
+#' @param hcadDataDir the folder with the HCAD raw parcels
+#' @param HCAD_parcelsRDS determines wheter to read preprocessed HCAD data from RDS file
+getHCADParcels <- function(hcadDataDir, HCAD_parcelsRDS = FALSE){
   
-  #Write to tab delimited file
-  #csv is a bad idea because of geometry column
-
-  saveRDS(validparceldataframe2,"validparceldataframe2.RDS")
-  saveRDS(validparcel2,"validparcelsforhouseholds.RDS")
-  #Write to tab delimited file
-
-  #So places where people may live in for group quarters I think are more likely to be in the commercial file
-  #then residential because it's commercial property-dorms, assisted living facilities etc
-
-  #So we need the non residential files
-  # The "building_other.txt" file can be downloaded from http://pdata.hcad.org/download/2014.html
-  buildingfeatures=read.table('building_other.txt',sep="\t", header=FALSE, fill=TRUE,colClasses = "character",strip.white = TRUE,quote = "")#, ,colClasses = "character",stringsAsFactors = FALSE
-  colnames(buildingfeatures)=c("ACCOUNT", "USE_CODE", "BUILDING_NUMBER","IMPRV_TYPE","BUILDING_STYLE_CODE","CLASS_STRUCTURE","CLASS_STRUC_DESCRIPTION","NOTICED_DEPR_VALUE","DEPRECIATION_VALUE","MS_REPLACEMENT_COST","CAMA_REPLACEMENT_COST","ACCRUED_DEPR_PCT","QUALITY","QUALITY_DESCRIPTION","DATE_ERECTED","EFFECTIVE_DATE","YR_REMODEL","YR_ROLL","APPRAISED_BY","APPRAISED_DATE","NOTE","IMPR_SQ_FT","ACTUAL_AREA","HEAT_AREA","GROSS_AREA","EFFECTIVE_AREA","BASE_AREA","PERIMETER","PERCENT_COMPLETE","CATEGORY","CATEGORY_DESC","PROPERTY_NAME","UNITS","NET_RENT_AREA","LEASE_RATE","OCCUPANCY","TOTAL_INCOME")
-
-  buildingfeatures$"HCAD_NUM"=buildingfeatures$ACCOUNT
-
-  validparcels3=geo_join(validparcels,buildingfeatures,by="HCAD_NUM",how="inner")
-  saveRDS(validparcels3,"validparcelsforgroupquartersmaybe.RDS")
-  #Convert to data frame
-  validparceldataframe3=as.data.frame(validparcels3)
+  if (HCAD_parcelsRDS) {
+    # the Parcels.shp file can be downloaded from http://pdata.hcad.org/GIS/index.html
+    parcels <- st_read(paste0(hcadDataDir, "Parcels/Parcels.shp"))
+    
+    # remove invalid parcels
+    parcels$valid <- st_is_valid(parcels)
+    HCAD_parcels <- subset(parcels,parcels$valid == "Valid Geometry")
+    HCAD_parcels$valid <- NULL
+    rm(parcels)
+    
+    # add tract information from Texas Census Tract Files
+    TXCensusTracts <- st_read(paste0(hcadDataDir, "gz_2010_48_150_00_500k/gz_2010_48_150_00_500k.shp"))
+    # put them in same CRS as Parcels
+    TXCensusTracts <- st_transform(TXCensusTracts, st_crs(HCAD_parcels))
+    
+    # match parcels with tracts
+    CensusTractforHCADParcels <- st_within(HCAD_parcels, TXCensusTracts)
+    CensusTractforHCADParcelsunlisted <- rapply(CensusTractforHCADParcels,function(x) ifelse(length(x)==0,9999999999999999999,x), how = "replace")
+    CensusTractforHCADParcelsunlisted <- unlist(CensusTractforHCADParcelsunlisted)
+    
+    # add census tract information to each parcel
+    HCAD_parcels$COUNTY=TXCensusTracts$COUNTY[CensusTractforHCADParcelsunlisted]
+    HCAD_parcelss$TRACT=TXCensusTracts$TRACT[CensusTractforHCADParcelsunlisted]
+    
+    # save as RDS
+    saveRDS(HCAD_parcels,paste0(hcadDataDir, "PreprocessedRDS/HCAD_parcels.RDS"))
+    
+    #cleanup
+    rm(TXCensusTracts)
+    rm(CensusTractforHCADParcels)
+    rm(CensusTractforHCADParcelsunlisted)
+    
+  } else {
+    HCAD_parcels <- readRDS(paste0(hcadDataDir, "PreprocessedRDS/HCAD_parcels.RDS"))
+  }
   
-  #Write to tab delimited file
-  #csv is a bad idea because of geometry column
-
-  saveRDS(validparceldataframe3,"validparceldataframe3forgroupquarters.RDS")
-
-  vp1=readRDS("validparcelsforhouseholds.RDS")
-  vp2=readRDS("validparcelsforgroupquartersmaybe.RDS")
-  library(plyr)
-  fullvalidparcelsweneed=rbind.fill(vp1,vp2)
-  saveRDS(fullvalidparcelsweneed,"valid_parcels_for_simulation.RDS")
+  # prepare parcels
+  HCAD_parcels <- HCAD_parcels %>% select(-BLK_NUM,-LOT_NUM,-CONDO_FLAG,-parcel_typ,-CurrOwner)
   
-  return (fullvalidparcelsweneed)
+  return(HCAD_parcels)
 }
 
-#add account number, remove duplictes, filter for building style codes
-prepareHCAD <- function() {
-  # create building account numbers
-  validparceldataframe2$ACCOUNT = paste0(validparceldataframe2$ACCOUNT, "_", validparceldataframe2$BUILDING_NUMBER)
-  validparceldataframe2 = validparceldataframe2[!duplicated(validparceldataframe2$ACCOUNT), ]
+#' getresBuildings function
+#'
+#' This function reads residential building information from HCAD (Harris County Aprraisal District) data files, joins valid parcels to create subset of residential building parcels and removes unnecessary columns.
+#'
+#' @param HCAD_parcels set of preprocessed parcels
+getresBuildings <- function(HCAD_parcels) {
+  # Prepare building features for residential houses downloaded from http://pdata.hcad.org/download/2014.html
+  HCAD_res_build <- read.csv2(paste0(hcadDataDir, "Real_building_land/building_res.txt"), 
+                              stringsAsFactors = FALSE,
+                              sep = "\t", header = FALSE, quote="",colClasses = c("V1"="character"))
+  # colnames are exported from HCAD access.zip
+  colnames(HCAD_res_build) <- readLines("Mappings/HCAD_ResBuildingFeaturesColumnNames.txt")
+  
+  HCAD_res_build$ACCOUNT <- str_remove_all(HCAD_res_build$ACCOUNT, " ")
+  HCAD_res_build <- HCAD_res_build %>%
+    select(ACCOUNT, IMPRV_TYPE, DEPRECIATION_VALUE, QUALITY_DESCRIPTION, DATE_ERECTED, YR_REMODEL,
+           EFFECTIVE_AREA, NBHD_FACTOR, SIZE_INDEX)
+  
+  # get residential parcels
+  Residentidential_Parcels <- tigris::geo_join(HCAD_parcels, HCAD_res_build_build, by_sp = "HCAD_NUM", by_df = "ACCOUNT", how = "inner")
+  
+  return(Residentidential_Parcels)
+}
+
+#' getrealBuildings function
+#'
+#' This function reads other building information from HCAD (Harris County Aprraisal District) data files, joins with valid parcels to create subset of real building parcels and removes unnecessary columns.
+#'
+#' @param HCAD_parcels set of preprocessed parcels
+getrealBuildings <- function(HCAD_parcels) {
+  # Prepare building features for other buildings e.g. dormitories etc. downloaded from http://pdata.hcad.org/download/2014.html
+  HCAD_real_build <- read.csv2(paste0(hcadDataDir, "Real_building_land/building_other.txt"), 
+                               sep = "\t", header = FALSE, quote="", colClasses = c("V1"="character"))
+  
+  # colnames are exported from HCAD access.zip
+  colnames(HCAD_real_build)  <- readLines("Mappings/HCAD_OthersBuildingFeaturesColumnNames.txt")
+  
+  HCAD_real_build$ACCOUNT <- str_remove_all(HCAD_real_build$ACCOUNT, " ")
+  HCAD_real_build <- HCAD_real_build %>% 
+    select(ACCOUNT,IMPRV_TYPE,DEPRECIATION_VALUE,QUALITY_DESCRIPTION,DATE_ERECTED,YR_REMODEL,BASE_AREA,
+           PERIMETER,PROPERTY_NAME,UNITS,LEASE_RATE,OCCUPANCY,TOTAL_INCOME)
+  
+  Realbuildigs_Parcels <- tigris::geo_join(HCAD_parcels, HCAD_real_build, by_sp = "HCAD_NUM", by_df = "ACCOUNT", how="inner")
+  
+  # remove duplicates by creating building account numbers
+  Realbuildigs_Parcels$ACCOUNT <- paste0(Realbuildigs_Parcels$ACCOUNT, "_", Realbuildigs_Parcels2$BUILDING_NUMBER)
+  Realbuildigs_Parcels <- Realbuildigs_Parcels[!duplicated(Realbuildigs_Parcels$ACCOUNT), ]
+  
+  return(Realbuildigs_Parcels)
+}
+
+
+
+#add account number, remove duplicates, filter for building style codes TO DO: Move into getrealBuildings function
+filterBuidingsHCAD <- function() {
+  
   
   # From the buildings in the tract, subset for only the ones with living spaces
   potential_buildings=subset(validparceldataframe2,validparceldataframe2$"BUILDING_STYLE_CODE" %in% c("101","102", "103", "104", "107","108","109","125","8177","8178","8179","8351","8354","8401","8548","8549","8550","8986","8988","105","8300","8352","8338","8459","8493","8546","8547","8596","8984","8987","8989", "660","8321","8324","8393","8424","8451","8589","8313","8322","8330","8335","8348","8394","8156","8551","8588","8710","8331","8309","8489","8311","8491","8514"))
--- main.R
+++ main.R
@@ -1,38 +1,152 @@
-#Main file to control creation of simulated city
+library(doParallel)
 
-source("getcensusdata.R")
-source("HCAD_Data.R")
+source("BaseScripts/Census_Data.R")
+source("BaseScripts/HCAD_Data.R")
+source("BaseScripts/helpers.R")
+source("BaseScripts/householdsGenerator.R")
+source("BaseScripts/groupquartersFunctions.R")
+source("BaseScripts/householdFunctions.R")
+source("BaseScripts/mergeWithHCAD.R")
+source("TestScripts/naCheck.R")
+source("TestScripts/proportionCheck.R")
+
 
 #set working directory to where all the data folders can be found
-setwd("~/University Of Houston/Price, Daniel M - Social Network Hypergraphs")
+datadir <- "~/University Of Houston/Price, Daniel M - Social Network Hypergraphs/"
 
-
-main <- function(censusFromRDS = TRUE, hcadFromRDS = TRUE) {
+#' createBaseSAM function
+#'
+#' This function controls the flow for simulating the base data (from census and hosuing stock)
+#'
+#' It calls other functions in the BaseScripts in order to simulate characteristics for households and for each individual
+#'
+#' @param censusFromRDS determines wheter to read preprocessed census data from an RDS file
+#' @param housingStockFromRDS determines whether to read preprcocessed housing stock data from an RDS file
+#' @param censusAPIKey The key needed to use the Census API (if data has to be generated)
+#' @return sam A dataframe of the simulated individuals in a city
+createBaseSAM <- function(censusFromRDS = TRUE, housingStockFromRDS = TRUE, censusAPIKey, numberOfCores = 1) {
   
   #get census data
   if (censusFromRDS) {
     # import saved RDS file 
-    Census_data <- readRDS("Census/Census_data_2014.RDS")
+    census_data <- readRDS(paste0(datadir, "Census/2014/Census_data_2014.RDS"))
   } else {
     # use the census API
-    Census_data <- census_data_API(base_url = 'http://api.census.gov/data/2014/acs5?', state = 48)
+    census_data <- census_data_viaAPI(base_url = 'http://api.census.gov/data/2014/acs5?', state = 48, censusAPIKey)
   }
   
-  # get building information (for Houston this data is provided by HCAD)
-  if (hcadFromRDS) {
-    # import saved RDS file 
-    Census_data <- readRDS("Census/Census_data_2014.RDS")
+  #get prepared complete housing parcels (with tract, county information), for Houston this data is provided by HCAD
+  if (housingStockFromRDS) {
+    HCAD_parcels <- readRDS(paste0(datadir, "HCAD/HCAD_resBuildings_2015.RDS"))
   } else {
-    # use the census API
-    Census_data <- census_data_API(base_url = 'http://api.census.gov/data/2014/acs5?', state = 48)
+    HCAD_parcels <- getHCADParcels(paste0(datadir,"HCAD/2015/"))
   }
-  i
-  #validparceldataframe2 <- prepareParcels(getHCADParcels())
+  
+  # get building information for residential buildings
+  if (housingStockFromRDS) {
+    # import saved RDS file 
+    res_housing_stock <- readRDS(paste0(datadir, "HCAD/2015/HCAD_resBuildings_2015.RDS"))
+  } else {
+    # preprocess HCAD data
+    res_housing_stock <- getresBuildings(paste0(datadir,"HCAD/2015/"))
+  }
+  
+  # get building information for other buildins 
+  if (housingStockFromRDS) {
+    # import saved RDS file 
+    realhousing_stock <- readRDS(paste0(datadir, "HCAD/2015/HCAD_realBuildings_2015.RDS"))
+  } else {
+    # preprocess HCAD data
+    realhousing_stock <- getrealBuildings(paste0(datadir,"HCAD/2015/"))
+  }
+  
+  
+  
+  #########REWRITE OF CODE NOT DONE FOR BELOW
+  
+  #set up for parallel processing
+  cl <- makeCluster(numberOfCores)
+  registerDoParallel(cl)
+  
+  # set the list of tracts
+  tracts = subset(census_data, census_data$county == 201)
+  
+  # generate households from the census data
+  households = foreach (index = tracts,.combine='rbind') %dopar% {
+    housholds = households_generator(48,201,tracts[index],seed=1,Census_data)
+    housholds$ACCOUNT=NA
+    return(households)
+  }
+  
+  # Now the people from the simulated model will be placed in households in the HCAD data
+  complete_sample_set=foreach (index = 1:786,.combine='rbind')%dopar%{
+    tract.sample.set = mergeWithHCAD(tracts[index], potential_houses, sample.set)
+    return(tract.sample.set)
+  }
+  
+  stopCluster(cl)
+  
+  return(complete_sample_set)
+  
+  #########REWRITE OF CODE NOT DONE FOR BELOW
+}
+
+#' sanityChecks function
+#'
+#' This function checks validity of base sam data
+#'
+#' @param complete_sample_set the generated sample set to be tested
+#' @param saveResults determines whether to to save test results to RDS files
+sanityChecks <- function(complete_sample_set, saveResults = TRUE) {
+  
+  #TODO: implement actual test not just check
+  result <- TRUE 
+  
+  # This tests if there are too many NAs in the model than expected. The resulting data frame is saved.
+  na = naTest(complete_sample_set)
+  # This tests the proportions in the model and compares them with the Census Data to check for accuracy. The resulting data frame is saved
+  prop = foreach (index = 1:786,.combine='rbind')%dopar%{
+    propByTract= prop_Check(sample.set, Census_data, tracts[index])
+    return(propByTract)
+  }
+  
+  if (saveRsults) {
+    saveRDS(na, "naCheck.RDS")
+    saveRDS(prop, "proportionCheck.RDS")
+  }
+  
+  return(result)
 }
 
 
 #HOW TO RUN
-sam <- main()
+sam <- createBaseSAM()
+
+# do some sanity checks and more columns
+if (sanityChecks(sam)) {
+  # Add column to add level to data
+  sam <- one_of(sam)
+  
+  # TODO move into HCAD preprocessing - adds cooordinates to the model based on the geometry column
+  sam <- add_lat_long(sam)
+  
+  # This converts all the columns in to the model to the appropriate class (either character or numeric)
+  sam <- convertColumnTypes(sam)
+  
+} else {
+  print("Did not pass sanity checks!")
+}
+
+#add extension columns - NHANES etc.
+
+
+
+#Save the final result
+saveRDS(sam, paste0("complete_sam", Sys.Date(), ".RDS"))
+
+
+
+
 
   
   
@@ -48,63 +162,18 @@ for(x in c("B1", "F1", "F2", "X1", "X2", "X3", "XE", "XJ", "XU")){
    potential_buildings$UNITS[potential_buildings$USE_CODE == x & potential_buildings$UNITS %in% c("", "A", "B", "C", "D", "1/1", "1/2")] = avg_units
 }
 
-# Everything else will be parallelized by the tract numbers
-tracts = subset(Census_data, Census_data$county == 201)
-tracts = tracts$tract
 
-# Set Up to run in Parallel
-library(doParallel)
 
-# If running on a super computer, request 10 nodes. Otherwise adjust the number.
-cl<-makeCluster(10)
-registerDoParallel(cl)
 
-source("householdsGenerator.R")
-source("groupquartersFunctions.R")
-source("individualFunctions.R")
-source("householdFunctions.R")
 
-# The indices can be adjusted
 
-# The citymodel functions are used to simulate all the people living in Houston
-sample.set=foreach (index = 1:786,.combine='rbind')%dopar%{
-  sample.set = households_generator(48,201,tracts[index],seed=1,inputdir = "../Inputs/",Census_data)
-  sample.set$ACCOUNT=NA
-  return(sample.set)
-}
 
-source("proportionCheck.R")
-# This tests the proportions in the model and compares them with the Census Data to check for accuracy. The resulting data frame is saved
-prop = foreach (index = 1:786,.combine='rbind')%dopar%{
-  propByTract= prop_Check(sample.set, Census_data, tracts[index])
-  return(propByTract)
-}
-saveRDS(prop, "proportionCheck.RDS")
 
-# Now the people from the simulated model will be placed in households in the HCAD data
-source("mergeWithHCAD.R")
-complete_sample_set=foreach (index = 1:786,.combine='rbind')%dopar%{
-  tract.sample.set = mergeWithHCAD(tracts[index], potential_houses, sample.set)
-  return(tract.sample.set)
-}
 
-stopCluster(cl)
 
-source("naCheck.R")
-# This tests if there are too many NAs in the model than expected. The resulting data frame is saved.
-na = naTest(complete_sample_set)
-saveRDS(na, "naCheck.RDS")
 
-source("one_of.R")
-# This numbers the rows in the model by powers of ten
-complete_sample_set = one_of(complete_sample_set)
 
-source("add_lat_long_sam.R")
-# This adds cooordinates to the model based on the geometry column
-complete_sample_set = add_lat_long(complete_sample_set)
 
-source("typeCheck.R")
-# This converts all the columns in to the model to the appropriate class (either character or numeric)
-complete_sample_set = typeCheck(complete_sample_set)
 
-saveRDS(complete_sample_set,paste0("complete_sample_set",Sys.Date(),".RDS"))
+
+
